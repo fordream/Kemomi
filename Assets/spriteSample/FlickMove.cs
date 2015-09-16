3fa8a008339ef0dfdd829fe97d0bfe72e2090648
杉本 貴章 <ybsgcn@gmail.com>
2015-09-16 16:31:19 +0900

    flick mukatuku test

---
+++ Assets/spriteSample/FlickMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickMove : MonoBehaviour {
+	private Vector3 prevMousePos;
+
+	// Use this for initialization
+	void Start () {
+		prevMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition );
+		Debug.Log (mousePos);
+		if ( Input.GetMouseButton(0) ) {
+			// マウスの移動分だけ速度与える
+			gameObject.GetComponent<Rigidbody2D>().velocity = (mousePos - prevMousePos) / Time.deltaTime;
+		}
+		
+		prevMousePos = mousePos;
+	}
+
+}
---
+++ Assets/spriteSample/FlickMove2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickMove2 : MonoBehaviour {
+
+//	Vector2 startPos;
+//	Vector2 direction;
+//	Vector2 endPos;
+	Vector2 prevPos;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (Input.touchCount > 0) {
+			var touch = Input.GetTouch(0);
+
+			switch(touch.phase){
+				case TouchPhase.Began:
+					// startPos = touch.position;
+					prevPos = touch.position;
+				break;
+				
+				case TouchPhase.Moved:
+				gameObject.GetComponent<Rigidbody2D>().velocity = touch.position - prevPos;
+					prevPos = touch.position;
+				break;
+				
+				case TouchPhase.Ended:
+					// endPos = touch.position;
+				break;
+			}
+		}
+	}
+}
