4a4f836bc38e586df4943235ddb365e6ce69fa84
杉本 貴章 <ybsgcn@gmail.com>
2015-09-16 17:53:53 +0900

    shuriken ppoi yatu

---
+++ Assets/spriteSample/FlickOnRight.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickOnRight : MonoBehaviour {
+
+	Vector2 startPos;
+	Vector2 endPos;
+	Vector2 direction;
+//	Vector2 prevPos;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (Input.touchCount > 0) {
+			var touch = Input.GetTouch(0);
+
+			switch(touch.phase){
+				case TouchPhase.Began:
+					startPos = touch.position;
+//					prevPos = touch.position;
+				break;
+				
+				case TouchPhase.Moved:
+//					gameObject.GetComponent<Rigidbody2D>().velocity = touch.position - prevPos;
+//					prevPos = touch.position;
+				break;
+				
+				case TouchPhase.Ended:
+					endPos = touch.position;
+					Vector2 direction = new Vector2(endPos.x - startPos.x, endPos.y - startPos.y);
+					float radian = Mathf.Atan2 ( direction.y , direction.x );
+					Debug.Log("magnitude : " + direction.magnitude);
+					if(direction.magnitude > 8) gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(4*Mathf.Cos(radian),4*Mathf.Sin(radian));
+				break;
+			}
+		}
+	}
+}
