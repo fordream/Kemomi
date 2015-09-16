1ed9c5ea58e4e830e1fe0b814321fda12953f040
onidora <onidora.c@gmail.com>
2015-09-16 22:56:31 +0900

    add touch UI

---
+++ Assets/PadTop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PadTop : MonoBehaviour {
+
+    TouchManager touchManager;
+    Renderer ren;
+    public float dis;
+
+    void Awake()
+    {
+        touchManager = GameObject.Find("touchManager").GetComponent<TouchManager>();
+        ren = gameObject.GetComponent<Renderer>();
+    }
+
+    void Start()
+    {
+        ren.enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (touchManager.leftAction == false)
+        {
+            ren.enabled = false;
+        }
+        else
+        {
+            ren.enabled = true;
+        }
+
+        transform.position = touchManager.basePoint + touchManager.leftDirection * dis;
+    }
+}
---
+++ Assets/StickCon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PadCon : MonoBehaviour {
+
+    TouchManager touchManager;
+
+
+	void Awake () {
+        touchManager = GameObject.Find("TouchManager").GetComponent<TouchManager>();
+	}
+
+    void Start()
+    {
+        GameObject.Find("padTop").GetComponent<Transform>().GetComponent<Renderer>().enabled = false;
+        GameObject.Find("padBase").GetComponent<Transform>();
+    }
+
+    void Update () {
+	    
+	}
+}
