40f7d0082d52ebde38f5407daced1b4889addc0e
onidora <onidora.c@gmail.com>
2015-09-16 17:43:48 +0900

    add multitap branch

---
+++ Assets/TouchManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour {
+
+    [SerializeField]
+    Vector2 leftDirection;
+
+    Vector2 basePoint;
+    bool leftAction = false;
+  
+
+    void Update()
+    {
+        foreach (var touch in Input.touches)
+        {
+            if (touch.position.x < Screen.width * 0.5)
+            {
+                leftCon(touch);
+                Debug.Log("touch left");
+            }
+
+            if (touch.position.x >= Screen.width * 0.5)
+            {
+                
+                //Debug.Log("touch right");
+            }
+
+        }
+    }
+
+    public void leftCon(Touch touch)
+    {
+        if (leftAction == false) {
+            basePoint = touch.position;
+            leftAction = true;
+            leftDirection = Vector2.zero;
+        }
+
+        if (leftAction)
+        {
+            leftDirection = (touch.position - basePoint).normalized;
+            if (touch.phase == TouchPhase.Ended)
+                leftAction = false;
+        }
+
+    }
+}
