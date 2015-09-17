e760fe00fc0a4589abb90755aff89207854b2d65
杉本 貴章 <ybsgcn@gmail.com>
2015-09-17 12:29:55 +0900

    inventory WIP 4

--- Assets/Scripts/ItemDB.cs
+++ Assets/Scripts/ItemDB.cs
@@ -7,13 +7,18 @@ public class ItemDB : MonoBehaviour
 	//全アイテムのリスト
 	public List<Item> items = new List<Item>();
 	
-	void Awake()
-	{
+	void Awake(){
 		// string name, int id, string desc, string itemIconPath
 		items.Add(new Item("手裏剣", 0, "投げてダメージを与える", "shuriken"));
 		items.Add(new Item("包帯", 1, "回復する", "houtai"));
 		items.Add(new Item("ダッシュブーツ", 2, "足が速くなる", "dashboots"));
-
+		items.Add(new Item("手裏剣2", 3, "投げてダメージを与える", "shuriken"));
+		items.Add(new Item("手裏剣3", 4, "投げてダメージを与える", "shuriken"));
+		items.Add(new Item("手裏剣4", 5, "投げてダメージを与える", "shuriken"));
+		items.Add(new Item("手裏剣5", 6, "投げてダメージを与える", "shuriken"));
+		items.Add(new Item("手裏剣6", 7, "投げてダメージを与える", "shuriken"));
+		items.Add(new Item("手裏剣7", 8, "投げてダメージを与える", "shuriken"));
+		items.Add(new Item("手裏剣8", 9, "投げてダメージを与える", "shuriken"));
 		
 	}
 }
