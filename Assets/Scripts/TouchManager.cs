3befe7c6b7f2be0602636357aa341b344cf57791
杉本 貴章 <ybsgcn@gmail.com>
2015-09-19 15:35:14 +0900

    InventoryManager merged with TouchManager

--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -15,14 +15,24 @@ public class TouchManager : MonoBehaviour
     public float flickTime;
     public bool flicked = false;
 
-    //==========非公開フィールド==========
+    //========== 非公開フィールド==========
     //右手操作に使う変数
     Vector2 startPos;
     Vector2 endPos;
     float beganTime = 0;
+	
+	public int fingerID_right;
+	public int fingerID_left;
 
+	//==========コンポーネントのハッシュ==========
+	InventoryManager inventoryManager;
 
     //==========基本コード==========
+
+	void Awake () {
+		inventoryManager = GameObject.Find("Inventory").GetComponent<InventoryManager>();
+	}
+
     void Update()
     {
         flicked = false;
@@ -30,20 +40,30 @@ public class TouchManager : MonoBehaviour
         //MULTIタップ管理、左画面タッチと右画面タッチの処理の分け
         foreach (var touch in Input.touches)
         {
-            if (touch.position.x < Screen.width * 0.5)
-            {
-                leftCon(touch);
-                Debug.Log("touch left");
-            }
 
-            if (touch.position.x >= Screen.width * 0.5)
-            {
-                rightCon(touch);
-                Debug.Log("touch right");
-            }
+			if(touch.phase == TouchPhase.Began){
+				if(inventoryManager.inventoryRect.Contains(new Vector2 (touch.position.x, Screen.height - touch.position.y))){
+					Debug.Log("touch inventory");
+				}
+	            else if (touch.position.x < Screen.width * 0.5 && fingerID_left == -1)
+	            {
+					fingerID_left = touch.fingerId;
 
+	                Debug.Log("touch left start fingerid =" + fingerID_left);
+	            }else if (touch.position.x >= Screen.width * 0.5 && fingerID_right == -1)
+	            {
+					fingerID_right = touch.fingerId;
+	                Debug.Log("touch right start fingerid =" + fingerID_right);
+	            }
+			}
+
+			if(fingerID_left == touch.fingerId) leftCon(touch);
+			if(fingerID_right == touch.fingerId) rightCon(touch);
         }
     }
+
+//	void onGUI(){
+//	}
 
     //左画面操作
     public void leftCon(Touch touch)
@@ -61,6 +81,7 @@ public class TouchManager : MonoBehaviour
             {
                 leftAction = false;
                 leftDirection = Vector2.zero;
+				fingerID_left = -1;
                 Debug.Log("TouchPhase.Ended");
             }
 		
@@ -87,6 +108,7 @@ public class TouchManager : MonoBehaviour
                 Vector2 direction = new Vector2(endPos.x - startPos.x, endPos.y - startPos.y);
                 float radian = Mathf.Atan2(direction.y, direction.x);
                 flickVector = new  Vector2( Mathf.Cos(radian), Mathf.Sin(radian));
+				fingerID_right = -1;
                 break;
 
         }
